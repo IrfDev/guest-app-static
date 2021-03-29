adebd4bf6841c9e992909b41f67b7b6ae89d50bc
Irving Suárez <irving.dev@icloud.com>
2021-03-29 12:24:26 -0600

    Building

---
+++ src/scripts/Index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  helloWorld: () => {
+    return {
+      cards: [
+        { colors: 'green', flipped: false },
+        { colors: 'red', flipped: false },
+        { colors: 'blue', flipped: false },
+        { colors: 'yellow', flipped: false },
+        { colors: 'magenta', flipped: false },
+      ],
+    };
+  },
+};
